29347b6166d192726a257f57f680f7306c8b0605
Murry Jeong (정문창) <comchangs@gmail.com>
2024-01-18 14:46:21 +0900

    Update set_ssh_key.sh

--- set_ssh_key.sh
+++ set_ssh_key.sh
@@ -2,4 +2,4 @@
 
 docker compose exec -it -u airflow airflow-worker /bin/bash -c 'mkdir -p /home/airflow/.ssh && echo -e "" > /home/airflow/.ssh/authorized_keys'
 docker compose cp ./ssh_key/id_rsa.pub airflow-worker:/home/airflow/.ssh/gitlab-runner.pub
-docker compose exec -it -u airflow airflow-worker /bin/bash -c 'ssh-keygen -A && cat /home/airflow/.ssh/gitlab-runner.pub >> /home/airflow/.ssh/authorized_keys && cat /home/airflow/.ssh/authorized_keys && chmod 700 ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys'
+docker compose exec -it -u airflow airflow-worker /bin/bash -c 'cat /home/airflow/.ssh/gitlab-runner.pub >> /home/airflow/.ssh/authorized_keys && cat /home/airflow/.ssh/authorized_keys && chmod 700 ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys'
