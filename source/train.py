df4eaff9fdb1f7289ec925d72593b8420394f685
Murry Jeong (정문창) <comchangs@gmail.com>
2024-01-18 14:44:29 +0900

    Update train.py

--- source/train.py
+++ source/train.py
@@ -1,12 +1,8 @@
 import pandas as pd
-import numpy as np
 from sqlalchemy import create_engine
 
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import learning_curve
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 
